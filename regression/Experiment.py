eef6da666707d6a72d05a299a15f302f68b7449b
dingmike001 <99937642+dingmike001@users.noreply.github.com>
2025-04-13 17:25:51 +0800

    Update Experiment.py

--- regression/Experiment.py
+++ regression/Experiment.py
@@ -15,139 +15,20 @@ from Network import LSTM, AttentionalLSTM, AttentionTransformer_Ning
 from utils import get_lr_scheduler, mean_percentage_error, mean_absolute_percentage_error, dataset_ss
 from scipy.io import savemat
 
-# torch.manual_seed(145)
+torch.manual_seed(145)
 warnings.filterwarnings("ignore")
 
 
-# train_data, test_data = utils.loaddata('/tmp/pycharm_project_LSTM/Dataset/6_20151121_noon_data_5.mat',
-#                                        '/tmp/pycharm_project_LSTM/Dataset/6_20151121_noon_perclos.mat')
-
-
-# train_data, val_data, test_data = utils.loaddatawithtest('/tmp/pycharm_project_LSTM/Dataset/6_20151121_noon_data_5.mat',
-#                                                          '/tmp/pycharm_project_LSTM/Dataset/6_20151121_noon_perclos.mat')
-
-
-#
-# epoch_num = 1000
-# channel_size = 17
-# data_dimention = 5
-# lays_num = 3
-# hidden_dimention = 12
-# label_size = 1
-#
-# model = Network.LSTMClsssifier(data_dimention, hidden_dimention, lays_num, label_size, channel_size)
-#
-# optimizer = optim.Adam(model.parameters(), lr=0.00001)
-
-# lossweight = torch.tensor([1, 1, 1]).float()
-# lossF = nn.CrossEntropyLoss(weight=lossweight)
-# lossF = nn.CrossEntropyLoss()
-# lossF = nn.MSELoss()
-# loss_array = []
-#
-# with trange(epoch_num, unit='iteration', desc='epoch') as pbar:
-#     for epoch in range(epoch_num):
-#         running_loss = 0
-#         for idx, content in enumerate(train_data):
-#             inputa = torch.tensor(content['eeg']).float()
-#             # labels = torch.tensor(content['label']).long()
-#             labels = torch.tensor(content['label']).float()
-#             output = model(inputa)
-#             optimizer.zero_grad()
-#             if labels.shape[0] == 1:
-#                 labels = labels.squeeze(0)
-#             else:
-#                 labels = labels.squeeze()
-#             loss = lossF(output, labels)
-#
-#             # loss = lossF(output, labels)
-#             loss.backward()
-#             optimizer.step()
-#             running_loss += loss.item()
-#         loss = running_loss / idx
-#         pbar.set_postfix(loss=loss)
-#         pbar.n += 1
-#         loss_array.append(loss)
-#
-# plt.plot(loss_array)
-# plt.show()
-#
-# testaccuracy = 0
-# drowisinessacc = 0
-# drowisiall = 0
-# awakeacc = 0
-# awakeall = 0
-# tiredacc = 0
-# tiredall = 0
-#
-# prict = []
-# label = []
-# with torch.no_grad():
-#     for idx, content in enumerate(val_data):
-#         inputa = torch.tensor(content['eeg']).float()
-#         # labels = torch.tensor(content['label']).long()
-#         labels = content['label']
-#         output = model(inputa)
-#         output = output[0][0]
-#         prict.append(output)
-#         label.append(labels)
-# inputa = torch.max(output, dim=1)
-# inputa = inputa.indices
-# inputa = np.array(inputa)
-# inputa = inputa[0]
-# labels = np.array(labels)[0][0]
-#         if labels == 2:
-#             drowisiall += 1
-#             if inputa == labels:
-#                 testaccuracy += 1
-#                 drowisinessacc += 1
-#         elif labels == 0:
-#             awakeall += 1
-#             if inputa == labels:
-#                 testaccuracy += 1
-#                 awakeacc += 1
-#         elif labels ==1:
-#             tiredall+=1
-#             if inputa == labels:
-#                 testaccuracy+=1
-#                 tiredacc+=1
-#         print("test ", idx, " probs:", output, " real label: ", labels, " accuracy number: ", testaccuracy)
-# testaccuracy = testaccuracy / (idx + 1)
-# awakeacc = awakeacc / awakeall
-# tiredacc = tiredacc/tiredall
-# if drowisiall ==0:
-#     drowisinessacc=0
-# else:
-#     drowisinessacc = drowisinessacc / drowisiall
-# print('Accuracy = ', testaccuracy)
-# print("Awake accuracy = ", awakeacc, "Awake number = ", awakeall)
-# print("Tired accuracy = ", tiredacc, "Tired number = ", tiredall)
-# print('Drowisiness accuracy = ', drowisinessacc, 'drowisiness number = ', drowisiall)
-
-# plt.plot(prict)
-# plt.plot(label)
-# plt.legend(['prict', 'label'])
-# plt.show()
 
 
 def main(args):
-    # train_data, test_data = utils.loaddata('./Dataset/train_de_2subjects.mat',
-    #                                        './Dataset/val_de_2subjects_1.mat')
-    # train_data, test_data = dataset_ss('./Dataset/seed_vig/1_20151124_noon_2_train.mat',
-    #                                    './Dataset/seed_vig/1_20151124_noon_2_val.mat')
-    # train_data, test_data = dataset_ss('./Dataset/seed_vig/1_20151124_noon_2_train.mat',
-    #                                    './Dataset/seed_vig/1_20151124_noon_2_val.mat')
+   
     train_data, test_data = dataset_ss(args.train_data,args.val_data)
 
-
-    # train_data, test_data = dataset_ss('./Dataset/val_de_23subjects_1.mat',
-    #                                    './Dataset/val_de_23subjects_5.mat')
     scaler = MinMaxScaler()
 
     # Lists #
     train_losses, train_lossesa, val_losses = list(), list(), list()
-    # val_maes, val_mses, val_rmses, val_mapes, val_mpes, val_r2s = list(), list(), list(), list(), list(), list()
-    # test_maes, test_mses, test_rmses, test_mapes, test_mpes, test_r2s = list(), list(), list(), list(), list(), list()
     pred_tests, labels = list(), list()
 
     # Constants #
@@ -160,7 +41,7 @@ def main(args):
     model.load_state_dict(torch.load('./Model/self_attention/2_20151106_noon.pkl', map_location='cuda'))
     model = model.to('cuda')
 
-    # model = AttentionalLSTM(input_size, qkv, hidden_size, num_layers, output_size, bidirectional)
+    
     # Loss Function #
     criterion = torch.nn.MSELoss()
 
@@ -263,11 +144,6 @@ def main(args):
                 # Forward Data #
                 pred_test = model(data)
 
-                # Convert to Original Value Range #
-                # pred_test, label = pred_test.detach().numpy(), label.detach().numpy()
-                #
-                # pred_test = scaler.inverse_transform(pred_test)
-                # label = scaler.inverse_transform(label)
 
                 if args.multi_step:
                     pred_test = np.mean(pred_test, axis=1)
@@ -276,30 +152,7 @@ def main(args):
                 pred_tests += pred_test.tolist()
                 labels += label.tolist()
 
-                # Calculate Loss #
-                # test_mae = mean_absolute_error(label, pred_test)
-                # test_mse = mean_squared_error(label, pred_test, squared=True)
-                # test_rmse = mean_squared_error(label, pred_test, squared=False)
-                # test_mpe = mean_percentage_error(label, pred_test)
-                # test_mape = mean_absolute_percentage_error(label, pred_test)
-                # test_r2 = r2_score(label, pred_test)
-
-                # Add item to Lists #
-                # test_maes.append(test_mae.item())
-                # test_mses.append(test_mse.item())
-                # test_rmses.append(test_rmse.item())
-                # test_mpes.append(test_mpe.item())
-                # test_mapes.append(test_mape.item())
-                # test_r2s.append(test_r2.item())
-
-            # Print Statistics #
-
-            # print(" MAE : {:.4f}".format(np.average(test_maes)))
-            # print(" MSE : {:.4f}".format(np.average(test_mses)))
-            # print("RMSE : {:.4f}".format(np.average(test_rmses)))
-            # print(" MPE : {:.4f}".format(np.average(test_mpes)))
-            # print("MAPE : {:.4f}".format(np.average(test_mapes)))
-            # # print(" R^2 : {:.4f}".format(np.average(test_r2s)))
+             
 
             print('MAE = ', mean_absolute_error(labels, pred_tests))
             print('R^2 = ', r2_score(labels, pred_tests))
@@ -323,11 +176,7 @@ def main(args):
             labels = {'label': labels}
             savemat('pred_tests.mat', pred_tests)
             savemat('label.mat', labels)
-            # np.save(os.path.join('/tmp/pycharm_project_LSTM/Model',
-            #                      '{}_using_{}_TestSet.npy'.format(model.__class__.__name__, step)),
-            #         np.asarray(pred_tests))
-            # np.save(os.path.join('/tmp/pycharm_project_LSTM/Model',
-            #                      'TestSet_using_{}.npy'.format(step)), np.asarray(labels))
+            
 
     elif args.mode == 'train and test':
         with trange(args.epoch_num, unit='iteration', desc='epoch') as pbar:
