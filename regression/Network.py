94fdf8f8778b7ce1ede5f8c982c6dd2083df42d5
dingmike001 <99937642+dingmike001@users.noreply.github.com>
2025-04-13 17:23:46 +0800

    Update Network.py

--- regression/Network.py
+++ regression/Network.py
@@ -5,77 +5,6 @@ from torch.autograd import Variable
 import math
 
 
-# class Attention(nn.Module):
-#     def __init__(self, hidden_size):
-#         super(Attention, self).__init__()
-#         self.hidden_size = hidden_size
-#         self.attn = nn.Linear(self.hidden_size, hidden_size)
-#         self.concat_linear = nn.Linear(self.hidden_size * 2, self.hidden_size)
-#
-#     def forward(self, lstm_outputs, final_hidden_state):
-#         batch_size, seq_len, _ = lstm_outputs.shape
-#         attn_weights = self.attn(lstm_outputs)
-#         attn_weights = torch.bmm(attn_weights, final_hidden_state.unsqueeze(2))
-#         attn_weights = F.softmax(attn_weights.squeeze(2), dim=1)
-#         context = torch.bmm(lstm_outputs.transpose(1, 2), attn_weights.unsqueeze(2)).squeeze(2)
-#         attn_hidden = torch.tanh(self.concat_linear(torch.cat((context, final_hidden_state), dim=1)))
-#         return attn_hidden, attn_weights
-#
-#
-# class LSTMClsssifier(nn.Module):
-#     def __init__(self, embedding_dim, hidden_dim, num_layers, label_size):
-#         super(LSTMClsssifier, self).__init__()
-#         self.embedding_dim = embedding_dim
-#         self.hidden_dim = hidden_dim
-#         self.num_layers = num_layers
-#         self.label_size = label_size
-#         self.lstmcal = nn.LSTM(self.embedding_dim, hidden_dim, num_layers=self.num_layers, batch_first=True)
-#         self.hidden2label = nn.Linear(self.hidden_dim, label_size)
-#         self.attn = Attention(hidden_dim)
-#         self.hidden = None
-#         self.rucal = nn.ReLU()
-#         self.linears = nn.ModuleList()
-#         self.linear_dims = [self.hidden_dim,5,3]
-#         for i in range(0, 2):
-#             self.linears.append(nn.Dropout(p=0.5))
-#             linear_layer = nn.Linear(self.linear_dims[i], self.linear_dims[i + 1])
-#             self.init_weights(linear_layer)
-#             self.linears.append(linear_layer)
-#             if i == len(self.linear_dims) - 1:
-#                 break  # no activation after output layer!!!
-#             self.linears.append(nn.ReLU())
-#
-#     def init_weights(self, layer):
-#         if type(layer) == nn.Linear:
-#             # print("Initialize layer with nn.init.xavier_uniform_: {}".format(layer))
-#             torch.nn.init.xavier_uniform_(layer.weight)
-#             layer.bias.data.fill_(0.01)
-#
-#
-#     def init_hidden(self, batch_size):
-#         return (torch.zeros((self.num_layers,batch_size, self.hidden_dim)),
-#                 torch.zeros((self.num_layers,batch_size,  self.hidden_dim)))
-#
-#     def forward(self, x):
-#         batch_size, seq_len, _ = x.shape
-#         h0,c0 = self.init_hidden(batch_size)
-#         lstm_output, self.hidden = self.lstmcal(x,(h0,c0))
-#         # final_state = self.hidden[0].view(self.num_layers, 1, batch_size, self.hidden_dim)[-1]
-#         # final_hidden_state = final_state.squeeze(0)
-#         # x, att_weights = self.attn(lstm_output, final_hidden_state)
-#         #
-#         # for l in self.linears:
-#         #     x = l(x)
-#         # x = self.hidden2label(x)
-#         # x = self.rucal(x)
-#         x = self.hidden2label(x)
-#
-#
-#         x = F.log_softmax(x, dim=1)
-#
-#
-#         return x
-
 
 class LSTMClsssifier(nn.Module):
     def __init__(self, input_size, hidden_dim, num_layers, label_size, seq_length):
@@ -102,14 +31,6 @@ class LSTMClsssifier(nn.Module):
         self.sof = nn.Softmax(dim=1)
         self.hidden2label = nn.Linear(self.hidden_dim, label_size)
         self.l_linear3 = nn.Linear(51, self.label_size)
-
-        # self.lstm1a = nn.LSTM(input_size=self.input_size, hidden_size=self.hidden_dim, num_layers=1,
-        #                       dropout=0, batch_first=True)
-        # self.lstm1b = nn.LSTM(input_size=hidden_dim, hidden_size=self.hidden_dim // 2, num_layers=1,
-        #                       dropout=0, batch_first=True)
-        # self.lstm1c = nn.LSTM(input_size=hidden_dim//2, hidden_size=self.hidden_dim // 4, num_layers=1,
-        #                       dropout=0, batch_first=True)
-        #
         self.linear1aa = nn.Linear(425, label_size)
 
     def init_hidden(self, batch_size):
@@ -141,33 +62,9 @@ class LSTMClsssifier(nn.Module):
         x = x.reshape(batch_size, -1)
         x = self.linear4th(x)
 
-
-
-
-
-
         return x
 
-#
-# class LSTMClsssifier(nn.Module):
-#     def __init__(self, input_size, hidden_dim, num_layers, label_size, seq_length):
-#         super(LSTMClsssifier, self).__init__()
-#         self.input_size = input_size
-#         self.hidden_dim = hidden_dim
-#         self.num_layers = num_layers
-#         self.label_size = label_size
-#         self.seq_length = seq_length
-#         self.lstmcal = nn.LSTM(input_size=input_size,hidden_size=hidden_dim,num_layers=num_layers,batch_first=True)
-#         self.fc = nn.Linear(hidden_dim,label_size)
-#
-#     def forward(self,x):
-#         batch,_,_=x.shape
-#         h_0 = Variable(torch.zeros(self.num_layers, batch, self.hidden_dim))
-#         c_0 = Variable(torch.zeros(self.num_layers, batch, self.hidden_dim))
-#         ula,(h_out,_) = self.lstmcal(x,(h_0,c_0))
-#         h_out = h_out.view(-1,self.hidden_dim)
-#         out = self.fc(h_out)
-#         return out
+
 
 class AttentionTransformer_Ning(nn.Module):
     def __init__(self, eeg_channel=17, d_model=32, n_head=8, d_hid=2048, n_layers=3, dropout=0.1,
